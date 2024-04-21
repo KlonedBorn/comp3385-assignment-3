e133e04885e244f37e14957c1c70ab792aa60d6e
KlonedBorn <michealdaivd132@gmail.com>
2024-04-21 17:02:14 -0400

    + Created routes and controllers

---
+++ app/Http/Controllers/PropertyCreateController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PropertyCreateController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
 // Create additional Routes below
